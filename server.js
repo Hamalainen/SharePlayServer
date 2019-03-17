25b2991be4d198c3ec6f8ab763a090cc80f6617f
Thomas Hämäläien <Hamalainen@github.com>
2019-03-17 17:57:35 +0100

    added playlist on server

--- server.js
+++ server.js
@@ -8,34 +8,50 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
-const playlists ={};
+let playlist = { playlist: [] };
 
 io.on('connection', (socket) => {
   let previousId;
   const safeJoin = currentId => {
-      socket.leave(previousId);
-      socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
-      previousId = currentId;
+    socket.leave(previousId);
+    socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
+    previousId = currentId;
   }
+
+
+      socket.on('updatePlayList', (video) => {
+        console.log('update server');
+        io.socket.emit('updateplaylist', video);
+      });
+
+  socket.on('addedToPlaylist', (video) => {
+    // if (!playlist.playlist.some((e) => e.id === video.id)) {
+      console.log('added: ' + video.id);
+      playlist.playlist.push(video.id);
+      io.sockets.emit('addedToPlaylist', video);
+    // }
+  });
+
+  socket
+
+  socket.emit('playlist', playlist);
 
   console.log(`Client ${socket.id} connected`);
   socket.on('disconnect', () => console.log(`Client ${socket.id} disconnected`));
 
-  let playlist = ['Y128KEvu6FI' , 'YgwOBoE5X1s'];
+
 
   socket.on('getPlayList', () => {
     console.log(`client ${socket.id} requested playlist:  ${playlist}`)
     socket.emit('playlist', {
-      playlist: 'Y128KEvu6FI'});
-});
-
-
-
+      playlist: 'Y128KEvu6FI'
+    });
+  });
 
 });
 
